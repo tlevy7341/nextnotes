d63559c5ad19531dfe20f0e1d5e9eb3315eba6c6
Tyrail Leverett <tyrailleverett@gmail.com>
2022-08-16 12:06:41 -0500

    initial commit

--- additional.d.ts
+++
@@ -1,9 +0,0 @@
-declare global {
-    namespace NodeJS {
-        interface ProcessEnv {
-            DATABASE_URL: string;
-            NEXTAUTH_SECRET: string;
-            JWT_SECRET: string;
-        }
-    }
-}
--- utils/helperfunctions.ts
+++
@@ -1,7 +0,0 @@
-import { AppUser } from "../types/types";
-import customAxios from "../utils/axios";
-
-export const getUser = async (id: number) => {
-    const response = await customAxios.post<AppUser>("/user/getuser", { id });
-    return response.data;
-};
